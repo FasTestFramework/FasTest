c6180796b3b50137a6049126a80f7cf7748265ec
Niket Sharma <Niket.Sharma@igglobal.com>
2020-03-06 00:34:32 -0600

    Swagger Dependency and Configuration

---
+++ src/main/java/com/infogain/automation/config/AutomationSwaggerConfig.java
@@ -0,0 +1,36 @@
+package com.infogain.automation.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import static springfox.documentation.builders.PathSelectors.regex;
+
+@Configuration
+@EnableSwagger2
+public class AutomationSwaggerConfig {
+
+    @Bean
+    public Docket deviceIntegrationApi() {
+        return new Docket(DocumentationType.SWAGGER_2).useDefaultResponseMessages(false).groupName("FasTest")
+                        .apiInfo(apiInfo()).select()
+                        .apis(RequestHandlerSelectors.basePackage("com.infogain.automation.controller"))
+                        .paths(regex("/.*")).build();
+    }
+    
+    public ApiInfo apiInfo() {
+
+        return new ApiInfoBuilder().title("FasTest Automation")
+                        .description("Testing Framework")
+                        .contact(new Contact("Infogain", "", "www.infogain.com")).version("0.1").build();
+
+    }
+    
+}
